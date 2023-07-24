63284102c57528d56b7a9c763668b6ca03ac3ee1
Michael Cazares <michaelcazares@gmail.com>
2023-07-24 08:04:06 -0700

    add Week 2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// function randomFraction() {
+//   let randomFraction = Math.random();
+//   return randomFraction;
+// }
+// console.log(Math.floor(Math.random() * 10));
+// console.log(randomFraction);
+
+const myElement = document.getElementById("email");
+myElement.style.color = "blue";
+
+const currDate = document.getElementById("currentDate");
+currDate.innerHTML = Date();
--- script.js
+++
@@ -1,6 +0,0 @@
-function randomFraction() {
-  let randomFraction = Math.random();
-  return randomFraction;
-}
-// console.log(Math.floor(Math.random() * 10));
-console.log(randomFraction);
